7728055e2b3033e6aa53139877064e66f48f3f22
filipemmr <112185872+Filipemrr@users.noreply.github.com>
2024-06-03 20:29:00 -0300

    FEAT: Fetching the API + dynamic generation of cards

---
+++ client/assets/js/brothCards.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const apiUrl = 'http://localhost:3000/meals/broth';
+
+    function createCard(data) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        card.innerHTML = `
+            <img src="${data.imageInactive}" alt="${data.name}">
+            <h2 class="card-name">${data.name}</h2>
+            <p class="card-description">${data.description}</p>
+            <h2 class="card-price">US$ ${data.price}</h2>
+        `;
+
+        return card;
+    }
+
+    function fetchBrothData() {
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(responseData => {
+                const data = responseData.data;
+                console.log("Data received:", data);
+                if (Array.isArray(data)) {
+                    const container = document.getElementById('broth-cards');
+                    container.innerHTML = '';
+                    data.forEach(item => {
+                        const card = createCard(item);
+                        container.appendChild(card);
+                    });
+                } else {
+                    console.error('Data is not an array:', data);
+                }
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    }
+
+    fetchBrothData();
+});
