eb232b88b1521cfbdb7680c49cfa526848387d7a
filipemmr <112185872+Filipemrr@users.noreply.github.com>
2024-06-06 00:04:36 -0300

    deploy

--- server/app.module.ts
+++ server/app.module.ts
@@ -11,7 +11,6 @@ import { ConfigModule } from '@nestjs/config';
         OrderModule,
         TypeOrmModule.forRoot({
             type: process.env.DB_TYPE as any,
-            host: process.env.PG_HOST,
             port: parseInt(process.env.PG_PORT),
             username: process.env.PG_USER,
             password: process.env.PG_PASSWORD,
